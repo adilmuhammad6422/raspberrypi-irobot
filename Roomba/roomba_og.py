07bfbda7debde1a98d9113394efd1c7288f1ba49
Adil Muhammad <35853750+adilmuhammad6422@users.noreply.github.com>
2024-07-05 14:22:55 -0400

    updated

--- Roomba/roomba_og.py
+++ Roomba/roomba_og.py
@@ -2,28 +2,26 @@ import serial
 import time
 
 def send(tty, commands):
-    for x in commands:
-        tty.write(bytes(x))
+    tty.write(bytearray(commands))
 
 def main():
-    tty = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=0.01)
+    tty = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=1)
 
     send(tty, [128, 132])
     time.sleep(1)
-    while 1:
-        time.sleep(100.0/1000.0)
+    while True:
+        time.sleep(0.1)
 
         send(tty, [149, 1, 7])
         inp = tty.read(1)
         if inp:
-            bump = ord(inp[0])
+            bump = ord(inp)
             if bump:
                 print("Bump, Rotating ...")
                 send(tty, [137, 0, 50, 0, 1])
                 time.sleep(0.1)
             else:
                 send(tty, [137, 0, 200, 128, 0])
-        tty.close()
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
