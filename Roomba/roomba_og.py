868571592caa87511ad49c553c1d462a1141649b
Adil Muhammad <35853750+adilmuhammad6422@users.noreply.github.com>
2024-07-05 14:25:22 -0400

    updated

--- Roomba/roomba_og.py
+++ Roomba/roomba_og.py
@@ -3,25 +3,32 @@ import time
 
 def send(tty, commands):
     tty.write(bytearray(commands))
+    print("Sent:", commands)
 
 def main():
     tty = serial.Serial(port='/dev/ttyUSB0', baudrate=57600, timeout=1)
 
-    send(tty, [128, 132])
+    send(tty, [128, 132])  # Start the robot in safe mode
     time.sleep(1)
+    
     while True:
         time.sleep(0.1)
 
-        send(tty, [149, 1, 7])
+        send(tty, [149, 1, 7])  # Request bumper sensor data
         inp = tty.read(1)
         if inp:
             bump = ord(inp)
-            if bump:
-                print("Bump, Rotating ...")
-                send(tty, [137, 0, 50, 0, 1])
+            print("Received:", bump, "Binary:", format(bump, '08b'))
+            
+            bump_right = bump & 0b00000001
+            bump_left = bump & 0b00000010
+            
+            if bump_right or bump_left:
+                print("Bump detected, Rotating ...")
+                send(tty, [137, 0, 50, 0, 1])  # Rotate command
                 time.sleep(0.1)
             else:
-                send(tty, [137, 0, 200, 128, 0])
+                send(tty, [137, 0, 200, 128, 0])  # Move forward command
 
 if __name__ == '__main__':
     main()
