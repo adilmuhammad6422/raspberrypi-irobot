b54ae219dc272f4b9e174c74ea3fdaeb66821349
Adil Muhammad <35853750+adilmuhammad6422@users.noreply.github.com>
2024-07-09 15:17:52 -0400

    upadated code

--- Server/client.py
+++ Server/client.py
@@ -1,30 +1,6 @@
-#import socket
-
-#HOST = "192.168.1.100"
-#PORT = 44700
-#client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#client_socket.connect((HOST,PORT))
-#while True:
-    #response = client_socket.recv(1024).decode('utf-8')
-    #if response == 'straignt':
-        #print("Roomba going Straight.")
-        #client_socket.sendall(str.encode('straight'))
-    #if response == 'left':
-        #print("Roomba going left.")
-        #client_socket.sendall(str.encode('left'))
-    #if response == 'right':
-        #print("Roomba going right.")
-        #client_socket.sendall(str.encode('right'))
-    #if response == 'quit':
-        #print("Quitting.")
-        #client_socket.sendall(str.encode('quit'))
-        #client_socket.close()
-        #break
-
-
 import socket
 
-HOST = "192.168.1.102"  # Change this to your server's IP address
+HOST = "192.168.1.102"  # Change this to your IP address
 PORT = 44700  # Port to connect to
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,10 +20,15 @@ try:
                 print("Roomba going right.")
                 client_socket.sendall(b'right')
             elif response == 'quit':
+                print("Stopping..")
+                client_socket.sendall(b'stop')
+                client_socket.shutdown(socket.SHUT_RDWR) # Stop both sending and receiving data using SHUT_RDWR
+            elif response == 'quit':
                 print("Quitting.")
                 client_socket.sendall(b'quit')
                 client_socket.close()
                 break
+
 finally:
     client_socket.close()
 
--- Server/server.py
+++ Server/server.py
@@ -1,22 +1,3 @@
-#import socket
-
-#HOST = "0.0.0.0"  # Listen on all available interfaces
-#PORT = 44700  # Port to listen on (non-privileged ports are > 1023)
-
-#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) #as s:
-    #s.bind((HOST, PORT))
-    #s.listen()
-    #print(f"Server is listening on {HOST}:{PORT}...")
-    #conn, addr = s.accept()
-    #with conn:
-        #print(f"Connected by {addr}")
-        #while True:
-            #msg_to_send = input("send a message:")
-            #conn.sendall(str.encode(msg_to_send))
-            #msg_received = conn.recv(1024).decode('utf-8')
-            #if msg_received == 'quit':
-                #break
-
 import socket
 import selectors
 import types
@@ -49,16 +30,16 @@ def service_connection(key, mask):
                 sock.close()
                 print("Closed connection to", data.addr)
                 del connections[sock]
-        else:
-            print("Closing connection to", data.addr)
-            sel.unregister(sock)
-            sock.close()
-            del connections[sock]
+        #else:
+            #print("Closing connection to", data.addr)
+            #sel.unregister(sock)
+            #sock.close()
+            #del connections[sock]
     if mask & selectors.EVENT_WRITE:
         if data.outb:
             print("Sending", data.outb.decode('utf-8'), "to", data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+            #sent = sock.send(data.outb)  # Should be ready to write
+            #data.outb = data.outb[sent:]
 
 # Set up the listening socket
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
