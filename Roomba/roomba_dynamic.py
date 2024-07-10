56d2c1f2839bbc84fae210d124e3f923f23b8e0c
Professional Account <nchatpolarak1@gmail.com>
2024-07-10 15:19:01 -0400

    up

--- Roomba/roomba_dynamic.py
+++ Roomba/roomba_dynamic.py
@@ -108,18 +108,18 @@ class Robot:
         print('Driving Straight with Bumper Detection...')
         self.drive_straight(2)
 
-        start_time = time.time()
-        while time.time() - start_time < duration:
-            bump_left, bump_right = self.detect_bumper()
+        # start_time = time.time()
+        # while time.time() - start_time < duration:
+        #     bump_left, bump_right = self.detect_bumper()
 
-            if bump_left:
-                print("Left bump detected, turning right...")
-                self.stop()
-            elif bump_right:
-                print("Right bump detected, turning left...")
-                self.stop()
+        #     if bump_left:
+        #         print("Left bump detected, turning right...")
+        #         self.stop()
+        #     elif bump_right:
+        #         print("Right bump detected, turning left...")
+        #         self.stop()
 
-        # Stop the robot
+        # # Stop the robot
         self.stop()
 
 
