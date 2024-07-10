cc253ce98b8cb31806caee5ef651df3913f55ea0
Professional Account <nchatpolarak1@gmail.com>
2024-07-10 14:06:17 -0400

    up

--- Roomba/roomba_dynamic.py
+++ Roomba/roomba_dynamic.py
@@ -118,7 +118,7 @@ class Robot:
 
         start_time = time.time()
         while time.time() - start_time < duration:
-            time.sleep(0.1)
+            #time.sleep(0.1)
 
             self.__write_command([149, 1, 7])  # Request bumper sensor data
             inp = self.tty.read(1)
