402fae5b5e5c3e6f2b8d41d58650b41e81ba4800
Professional Account <nchatpolarak1@gmail.com>
2024-07-11 16:11:59 -0400

    Added comments to roomba

--- Roomba-Backups/roomba_dynamic.py
+++ Roomba-Backups/roomba_dynamic.py
@@ -2,27 +2,38 @@ import serial
 import time
 import sys
 
-# Robot class contains the implementation and calling functions for the Roomba
+# Robot class contains the implementation and calling functions for the roomba
 class Robot:
+    
     def __init__(self, port='/dev/ttyUSB0', baudrate=57600, timeout=0.01, velocity=200):
         self.tty = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
         self.velocity = velocity
-        self.bump_thresh = 0.1  # 100 ms (converted to seconds for time.sleep())
-        self.bump_left_time = self.bump_right_time = None
 
+        self.bump_thresh = 0.1 # 0.1 seconds
+        self.bump_left_start_time = self.bump_right_start_time = None
+    
+    # Sets the velocity of the robot
     def set_velocity(self, velocity):
-        print("Setting velocity to:", velocity)
+        print("Setting velocity to:", velocity)  # Debugging print
         self.velocity = velocity
 
+    # Writes Commands to the tty
     def __write_command(self, commands):
+        # print("Sending commands:", commands)  # Debugging print
         for x in commands:
             self.tty.write(bytes([x]))
-
+    
+    # Calls robot commands
     def __call_command(self, radius):
+        # Convert velocity and radius to bytes
         vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte = self.__convert_to_bytes(self.velocity, radius)
+        
+        # Send drive command
         command = [137, vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte]
         self.__write_command(command)
 
+    # Helper function to convert velocity and radius 
+    # to high bytes and low bytes for the roomba to understand
     def __convert_to_bytes(self, velocity, radius):
         vel_high_byte = (velocity >> 8) & 0xFF
         vel_low_byte = velocity & 0xFF
@@ -30,36 +41,54 @@ class Robot:
         radius_low_byte = radius & 0xFF
         return vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte
 
+    # Function that makes the robot drive straight for a set duration
     def drive_straight(self, duration):
         print('Driving Straight...')
-        self.__call_command(32768)  # Calls drive command, 32768 is the radius for driving straight
-        time.sleep(duration)
 
+        self.__call_command(32768) # Calls drive command, 32768 is the radius for driving straight
+        time.sleep(duration) # Wait for the specified duration
+
+    # Function for turning x degrees
+    # Negative angle for turning left, positive angles for turning right
     def turn_dynamic_angle(self, angle):
         print('Turning ' + str(angle) + ' degree')
+
+        # If the angle is 0 drive straight, otherwise turn porportionally for the given angle
         if angle == 0:
             radius = 32768
         else:
             radius = 1 / (angle / 90.0)
+
+
         self.__call_command(int(radius))
+        
+        # Waits for the angle to turn
         time_to_turn = abs(angle) / 90.0
+        # time.sleep(time_to_turn)
         time.sleep(time_to_turn)
 
+    # Stops the robot
     def stop(self):
         print("Stopping the robot...")
-        self.__write_command([137, 0, 0, 0, 0])
+        self.__write_command([137, 0, 0, 0, 0]) # Sets the velocity and radius to 0
 
+    # Function for turning the robot while its driving
+    # It turns the given radius for a given duration
     def turn_while_driving(self, radius, duration):
         print('Turns left while driving...')
         self.drive_straight(duration)
         self.__call_command(radius)
+        
+        # Sleeps for the time it takes to turn
         time.sleep(duration)
 
+    # Starts the robot to accept commands
     def start(self):
-        print("Starting the robot")
+        print("Starting the robot")  # Debugging print
         self.__write_command([128, 132])
         time.sleep(1)
 
+    # Function to detect if it's a left bump or right bump
     def detect_bumper(self):
         self.__write_command([149, 1, 7])  # Request bumper sensor data
         inp = self.tty.read(1)
@@ -69,45 +98,28 @@ class Robot:
             bump_left = bump & 0b00000010
             
             if bump_right:
-                if self.bump_right_time is None:
-                    self.bump_right_time = time.time()
-                elif time.time() - self.bump_right_time > self.bump_thresh:
-                    print('Bump right detected for:', self.bump_right_time)
-                    self.bump_right_time = None
+                if self.bump_right_start_time is None:
+                    self.bump_right_start_time = time.time()
+                elif time.time() - self.bump_right_start_time > self.bump_thresh:
+                    print('Bump right detected for:', time.time() - self.bump_right_start_time)
+                    self.bump_right_start_time = None
                     return False, True  # right bump detected
             else:
-                self.bump_right_time = None
+                self.bump_right_start_time = None
             
             if bump_left:
-                if self.bump_left_time is None:
-                    self.bump_left_time = time.time()
-                elif time.time() - self.bump_left_time > self.bump_thresh:
-                    print('Bump left detected for:', self.bump_left_time)
-                    self.bump_left_time = None
+                if self.bump_left_start_time is None:
+                    self.bump_left_start_time = time.time()
+                elif time.time() - self.bump_left_start_time > self.bump_thresh:
+                    print('Bump left detected for:', time.time() - self.bump_left_start_time)
+                    self.bump_left_start_time = None
                     return True, False  # left bump detected
             else:
-                self.bump_left_time = None
+                self.bump_left_start_time = None
 
         return False, False  # no bump detected
-
-    def turn_left(self, duration=1):
-        print('Turning left...')
-        radius = 1  # Special code for turning in place counterclockwise
-        vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte = self.__convert_to_bytes(self.velocity, radius)
-        turn_command = [137, vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte]
-        self.__write_command(turn_command)
-        time.sleep(duration)
-        self.stop()
-
-    def turn_right(self, duration=1):
-        print('Turning right...')
-        radius = -1  # Special code for turning in place clockwise
-        vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte = self.__convert_to_bytes(self.velocity, radius)
-        turn_command = [137, vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte]
-        self.__write_command(turn_command)
-        time.sleep(duration)
-        self.stop()
-
+    
+    # Function for driving straight with bumper detection
     def drive_straight_with_bumper_detection(self, duration, angle_to_turn):
         # duration: seconds. the amount of time this test should run for
         # angle_to_turn: degrees. the angle to turn (left or right) when a bump is detected 
@@ -127,11 +139,15 @@ class Robot:
                 driving_forward = False
                 self.stop()
                 self.turn_dynamic_angle(angle_to_turn)
+                # self.turn_right(duration=0.5)
+                # break
             elif bump_right:
                 print("Right bump detected, turning left...")
                 driving_forward = False
                 self.stop()
                 self.turn_dynamic_angle(-angle_to_turn)
+                # self.turn_left(duration=0.5)
+                # break
             else:
                 if not driving_forward:
                     self.__write_command(drive_command)
@@ -141,33 +157,61 @@ class Robot:
 
 # Testing methods
 
+# Test method for driving and turning
 def test_drive_and_turn(robot):
-    robot.drive_straight(2)
-    robot.stop()
-    robot.turn_dynamic_angle(-90)
-    robot.stop()
-    robot.drive_straight(3)
-    robot.stop()
-    robot.turn_dynamic_angle(90)
-    robot.stop()
-    robot.drive_straight(2)
-    robot.stop()
-    robot.turn_dynamic_angle(45)
-    robot.stop()
-    robot.drive_straight(2)
-    robot.stop()
+        # Drive straight for 2 seconds
+        robot.drive_straight(2)
+        robot.stop()
+    
+        # Turn left
+        robot.turn_dynamic_angle(-90)
+        robot.stop()
 
+        # Drive straight for another 2 seconds
+        robot.drive_straight(3)
+        robot.stop()
+
+        # Turn right
+        robot.turn_dynamic_angle(90)
+        robot.stop()
+
+        # Drive straight for another 2 seconds
+        robot.drive_straight(2)
+        robot.stop()
+
+        # Turn dynamically by 45 degrees
+        robot.turn_dynamic_angle(45)
+        robot.stop()
+
+        # Drive straight for another 2 seconds
+        robot.drive_straight(2)
+        
+
+        # Stop the robot after driving straight
+        robot.stop()
+
+# Test method for turning WHILE driving
 def test_turn_while_driving(robot):
-    robot.drive_straight(2)
-    robot.turn_while_driving(-500, 2)
-    robot.turn_while_driving(500, 2)
-    robot.stop()
+        # Drive straight for another 2 seconds
+        robot.drive_straight(2)
 
+        # Turn left while driving with a radius of 500mm for 2 seconds
+        robot.turn_while_driving(-500, 2)
+
+        # Turn right while driving with a radius of 500mm for 2 seconds
+        robot.turn_while_driving(500, 2)
+
+        # Stop the robot after driving straight
+        robot.stop()
+
+# The main method
 def main():
     robot = Robot()
     robot.start()
     robot.set_velocity(200)
-    robot.drive_straight_with_bumper_detection(30, 90)
+    test_drive_and_turn(robot)
+    
 
+# Calls the main method
 if __name__ == '__main__':
     main()
--- Roomba/roomba_dyna.py
+++ Roomba/roomba_dyna.py
@@ -1,38 +1,40 @@
 import serial
 import time
-import sys
 
-# Robot class contains the implementation and calling functions for the roomba
+# Robot class contains the implementation and calling functions for the Roomba
 class Robot:
     # Initializes the tty and velocity from the parameters
     # default port is '/dev/ttyUSB0'
     # default baudrate is 57600
     # default timeout is 0.01
     # default velocity is 200
+    #
+    # Set the bump threshold to be 0.1 secs, 
+    # which is how long the bumper must be pressed to count as a bump
+    # The bump start left and right times are none at the beginning of the program
     def __init__(self, port='/dev/ttyUSB0', baudrate=57600, timeout=0.01, velocity=200):
         self.tty = serial.Serial(port=port, baudrate=baudrate, timeout=timeout)
         self.velocity = velocity
+        self.bump_thresh = 0.1  # in seconds
+        self.bump_start_left_time = self.bump_start_right_time = None
 
-        self.bump_thresh = 0.1 # 0.1 seconds
-        self.bump_left_start_time = self.bump_right_start_time = None
-    
-    # Sets the velocity of the robot
+    # Sets the velocity of the robot to the given velocity
     def set_velocity(self, velocity):
-        print("Setting velocity to:", velocity)  # Debugging print
+        print('Setting velocity to: ', velocity, 'mm/s')
         self.velocity = velocity
 
     # Writes Commands to the tty
     def __write_command(self, commands):
-        # print("Sending commands:", commands)  # Debugging print
         for x in commands:
             self.tty.write(bytes([x]))
-    
-    # Calls robot commands
+
+     # Calls robot commands
     def __call_command(self, radius):
         # Convert velocity and radius to bytes
         vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte = self.__convert_to_bytes(self.velocity, radius)
-        
+
         # Send drive command
+        # 137 byte is for drive
         command = [137, vel_high_byte, vel_low_byte, radius_high_byte, radius_low_byte]
         self.__write_command(command)
 
@@ -48,33 +50,32 @@ class Robot:
     # Function that makes the robot drive straight for a set duration
     def drive_straight(self, duration):
         print('Driving Straight...')
-
-        self.__call_command(32768) # Calls drive command, 32768 is the radius for driving straight
-        time.sleep(duration) # Wait for the specified duration
+        self.__call_command(32768)  # Calls drive command, 32768 is the radius for driving straight
+        time.sleep(duration)
 
     # Function for turning x degrees
     # Negative angle for turning left, positive angles for turning right
     def turn_dynamic_angle(self, angle):
         print('Turning ' + str(angle) + ' degree')
 
-        # If the angle is 0 drive straight, otherwise turn porportionally for the given angle
+         # If the angle is 0 drive straight, 
+         # otherwise turn porportionally for the given angle
         if angle == 0:
             radius = 32768
         else:
             radius = 1 / (angle / 90.0)
-
-
         self.__call_command(int(radius))
-        
-        # Waits for the angle to turn
+
+         # Waits for the angle to turn
         time_to_turn = abs(angle) / 90.0
+
         # time.sleep(time_to_turn)
         time.sleep(time_to_turn)
 
-    # Stops the robot
+     # Stops the robot from moving
     def stop(self):
         print("Stopping the robot...")
-        self.__write_command([137, 0, 0, 0, 0]) # Sets the velocity and radius to 0
+        self.__write_command([137, 0, 0, 0, 0])
 
     # Function for turning the robot while its driving
     # It turns the given radius for a given duration
@@ -82,17 +83,17 @@ class Robot:
         print('Turns left while driving...')
         self.drive_straight(duration)
         self.__call_command(radius)
-        
+
         # Sleeps for the time it takes to turn
         time.sleep(duration)
 
-    # Starts the robot to accept commands
+     # Starts the robot to accept commands
     def start(self):
-        print("Starting the robot")  # Debugging print
+        print("Starting the robot...")
         self.__write_command([128, 132])
         time.sleep(1)
 
-    # Function to detect if it's a left bump or right bump
+    # Function to detect if a bump has been detected
     def detect_bumper(self):
         self.__write_command([149, 1, 7])  # Request bumper sensor data
         inp = self.tty.read(1)
@@ -102,27 +103,27 @@ class Robot:
             bump_left = bump & 0b00000010
             
             if bump_right:
-                if self.bump_right_start_time is None:
-                    self.bump_right_start_time = time.time()
-                elif time.time() - self.bump_right_start_time > self.bump_thresh:
-                    print('Bump right detected for:', time.time() - self.bump_right_start_time)
-                    self.bump_right_start_time = None
+                if self.bump_start_right_time is None:
+                    self.bump_start_right_time = time.time()
+                elif time.time() - self.bump_start_right_time > self.bump_thresh:
+                    print('Bump right detected for:',  time.time() - self.bump_start_right_time)
+                    self.bump_start_right_time = None
                     return False, True  # right bump detected
             else:
-                self.bump_right_start_time = None
+                self.bump_start_right_time = None
             
             if bump_left:
-                if self.bump_left_start_time is None:
-                    self.bump_left_start_time = time.time()
-                elif time.time() - self.bump_left_start_time > self.bump_thresh:
-                    print('Bump left detected for:', time.time() - self.bump_left_start_time)
-                    self.bump_left_start_time = None
+                if self.bump_start_left_time is None:
+                    self.bump_start_left_time = time.time()
+                elif time.time() - self.bump_start_left_time > self.bump_thresh:
+                    print('Bump left detected for:', time.time() - self.bump_start_left_time)
+                    self.bump_start_left_time = None
                     return True, False  # left bump detected
             else:
-                self.bump_left_start_time = None
+                self.bump_start_left_time = None
 
         return False, False  # no bump detected
-    
+
     # Function for driving straight with bumper detection
     def drive_straight_with_bumper_detection(self, duration, angle_to_turn):
         # duration: seconds. the amount of time this test should run for
@@ -135,23 +136,21 @@ class Robot:
         driving_forward = True
 
         start_time = time.time()
+
+        # Runs the robot for duration (seconds)
         while time.time() - start_time < duration:
-            time.sleep(0.1)
+            time.sleep(0.1) # sleep for 0.1 seconds
             bump_left, bump_right = self.detect_bumper()
             if bump_left:
                 print("Left bump detected, turning right...")
                 driving_forward = False
                 self.stop()
                 self.turn_dynamic_angle(angle_to_turn)
-                # self.turn_right(duration=0.5)
-                # break
             elif bump_right:
                 print("Right bump detected, turning left...")
                 driving_forward = False
                 self.stop()
                 self.turn_dynamic_angle(-angle_to_turn)
-                # self.turn_left(duration=0.5)
-                # break
             else:
                 if not driving_forward:
                     self.__write_command(drive_command)
@@ -161,60 +160,47 @@ class Robot:
 
 # Testing methods
 
-# Test method for driving and turning
 def test_drive_and_turn(robot):
-        # Drive straight for 2 seconds
-        robot.drive_straight(2)
-        robot.stop()
+    # Drive straight for 2 seconds
+    robot.drive_straight(2)
+    robot.stop()
+
+    # Turn 90 degrees to the right
+    robot.turn_dynamic_angle(-90)
+    robot.stop()
+
+    # Drives straight for 3 seconds
+    robot.drive_straight(3)
+    robot.stop()
+
+    # Turn 90 degrees to the left
+    robot.turn_dynamic_angle(90)
+    robot.stop()
+
+    # Drive straight for 2 seconds
+    robot.drive_straight(2)
+    robot.stop()
     
-        # Turn left
-        robot.turn_dynamic_angle(-90)
-        robot.stop()
+    # Turn 45 degrees to the left
+    robot.turn_dynamic_angle(45)
+    robot.stop()
 
-        # Drive straight for another 2 seconds
-        robot.drive_straight(3)
-        robot.stop()
+    # Drive straight for 2 seconds
+    robot.drive_straight(2)
+    robot.stop()
 
-        # Turn right
-        robot.turn_dynamic_angle(90)
-        robot.stop()
-
-        # Drive straight for another 2 seconds
-        robot.drive_straight(2)
-        robot.stop()
-
-        # Turn dynamically by 45 degrees
-        robot.turn_dynamic_angle(45)
-        robot.stop()
-
-        # Drive straight for another 2 seconds
-        robot.drive_straight(2)
-        
-
-        # Stop the robot after driving straight
-        robot.stop()
-
-# Test method for turning WHILE driving
 def test_turn_while_driving(robot):
-        # Drive straight for another 2 seconds
-        robot.drive_straight(2)
+    robot.drive_straight(2) # Drive straight for 2 seconds
+    robot.turn_while_driving(-500, 2) # Gradually turn right 500 mm for 2 secs
+    robot.turn_while_driving(500, 2) # Gradually turn left 500 mm for 2 secs
+    robot.stop() # Stop the robot
 
-        # Turn left while driving with a radius of 500mm for 2 seconds
-        robot.turn_while_driving(-500, 2)
-
-        # Turn right while driving with a radius of 500mm for 2 seconds
-        robot.turn_while_driving(500, 2)
-
-        # Stop the robot after driving straight
-        robot.stop()
-
-# The main method
+# The main method to call robot functions
 def main():
     robot = Robot()
     robot.start()
     robot.set_velocity(200)
-    test_drive_and_turn(robot)
-    
+    robot.drive_straight_with_bumper_detection(30, 90)
 
 # Calls the main method
 if __name__ == '__main__':
