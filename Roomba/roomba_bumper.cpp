bc8760e7976636ea66f09e1c63ecbf4ee30d7e28
Professional Account <nchatpolarak1@gmail.com>
2024-07-25 15:51:43 -0400

    Testing drive straight

--- Roomba/roomba_bumper.cpp
+++ Roomba/roomba_bumper.cpp
@@ -1,48 +1,20 @@
 #include "create/create.h"
 
+#include <iomanip>
 #include <iostream>
-#include <thread>
-#include <chrono>
-
-void driveStraight(create::Create &robot, int duration_ms)
-{
-    robot.drive(0.2, 0); // Drive straight at 0.2 m/s
-    std::this_thread::sleep_for(std::chrono::milliseconds(duration_ms));
-    robot.drive(0, 0); // Stop
-}
-
-void turnRight(create::Create &robot)
-{
-    robot.drive(0, -M_PI / 4);                            // Turn right at -45 degrees per second
-    std::this_thread::sleep_for(std::chrono::seconds(2)); // Turn for 2 seconds
-    robot.drive(0, 0);                                    // Stop
-}
-
-void turnLeft(create::Create &robot)
-{
-    robot.drive(0, M_PI / 4);                             // Turn left at 45 degrees per second
-    std::this_thread::sleep_for(std::chrono::seconds(2)); // Turn for 2 seconds
-    robot.drive(0, 0);                                    // Stop
-}
 
 int main(int argc, char **argv)
 {
-    create::RobotModel model = create::RobotModel::CREATE_2;
+    // Make Create 1 robot
+    create::RobotModel model = create::RobotModel::CREATE_1;
     std::string port = "/dev/ttyUSB0";
-    int baud = 115200;
-    if (argc > 1 && std::string(argv[1]) == "create1")
-    {
-        model = create::RobotModel::CREATE_1;
-        baud = 57600;
-        std::cout << "Running driver for Create 1" << std::endl;
-    }
-    else
-    {
-        std::cout << "Running driver for Create 2" << std::endl;
-    }
+    int baud = 57600;
+    std::cout << "Running driver for Create 1" << std::endl;
 
+    // Construct robot object
     create::Create robot(model);
 
+    // Connect to robot
     if (robot.connect(port, baud))
         std::cout << "Connected to robot" << std::endl;
     else
@@ -51,25 +23,26 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // Switch to Full mode
     robot.setMode(create::MODE_FULL);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // There's a delay between switching modes and when the robot will accept drive commands
+    usleep(100000);
+
+    // Command robot to drive a radius of 0.15 metres at 0.2 m/s
+    robot.drive(2, 0);
+
+    // robot.driveRadius(0.2, 0.15);
 
     while (true)
     {
-        driveStraight(robot, 2000); // Drive straight for 2 seconds
+        // Get robot odometry and print
+        const create::Pose pose = robot.getPose();
 
-        if (robot.isLeftBumper())
-        {
-            std::cout << "Left bumper hit, turning right" << std::endl;
-            turnRight(robot);
-        }
-        else if (robot.isRightBumper())
-        {
-            std::cout << "Right bumper hit, turning left" << std::endl;
-            turnLeft(robot);
-        }
+        std::cout << std::fixed << std::setprecision(2) << "\rOdometry (x, y, yaw): ("
+                  << pose.x << ", " << pose.y << ", " << pose.yaw << ")      ";
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Small delay to avoid busy waiting
+        usleep(10000); // 10 Hz
     }
 
     return 0;
