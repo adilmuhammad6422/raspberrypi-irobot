9421c786e43c5bb0281cfde85d8ddeedfa5bb6b6
Nathan Chatpolarak <nchatpolarak1@gmail.com>
2024-07-25 16:46:50 -0400

    Updated

--- Roomba/roomba_bumper.cpp
+++ Roomba/roomba_bumper.cpp
@@ -3,54 +3,49 @@
 #include <iomanip>
 #include <iostream>
 
-void driveStraight(create::Create &robot, int duration_ms, double velocity)
-{
-    robot.drive(velocity, 0); // Drive straight at 0.2 m/s
-    std::this_thread::sleep_for(std::chrono::milliseconds(duration_ms));
-    robot.drive(0, 0); // Stop
-}
-
-int main(int argc, char **argv)
-{
-    // Make Create 1 robot
-    create::RobotModel model = create::RobotModel::CREATE_1;
-    std::string port = "/dev/ttyUSB0";
-    int baud = 57600;
+int main(int argc, char** argv) {
+  // Select robot. Assume Create 2 unless argument says otherwise
+  create::RobotModel model = create::RobotModel::CREATE_2;
+  std::string port = "/dev/ttyUSB0";
+  int baud = 115200;
+  if (argc > 1 && std::string(argv[1]) == "create1") {
+    model = create::RobotModel::CREATE_1;
+    baud = 57600;
     std::cout << "Running driver for Create 1" << std::endl;
+  }
+  else {
+    std::cout << "Running driver for Create 2" << std::endl;
+  }
 
-    // Construct robot object
-    create::Create robot(model);
+  // Construct robot object
+  create::Create robot(model);
 
-    // Connect to robot
-    if (robot.connect(port, baud))
-        std::cout << "Connected to robot" << std::endl;
-    else
-    {
-        std::cout << "Failed to connect to robot on port " << port.c_str() << std::endl;
-        return 1;
-    }
+  // Connect to robot
+  if (robot.connect(port, baud))
+    std::cout << "Connected to robot" << std::endl;
+  else {
+    std::cout << "Failed to connect to robot on port " << port.c_str() << std::endl;
+    return 1;
+  }
 
-    // Switch to Full mode
-    robot.setMode(create::MODE_FULL);
+  // Switch to Full mode
+  robot.setMode(create::MODE_FULL);
 
-    // There's a delay between switching modes and when the robot will accept drive commands
-    usleep(100000);
+  // There's a delay between switching modes and when the robot will accept drive commands
+  usleep(100000);
 
-    // Command robot to drive a radius of 0.15 metres at 0.2 m/s
-    driveStraight(robot, 2, 2.0);
+  // Command robot to drive a radius of 0.15 metres at 0.2 m/s
+  robot.driveRadius(0.2, 0.15);
 
-    // robot.driveRadius(0.2, 0.15);
+  while (true) {
+    // Get robot odometry and print
+    const create::Pose pose = robot.getPose();
 
-    while (true)
-    {
-        // Get robot odometry and print
-        const create::Pose pose = robot.getPose();
+    std::cout << std::fixed << std::setprecision(2) << "\rOdometry (x, y, yaw): ("
+              << pose.x << ", " << pose.y << ", " << pose.yaw << ")      ";
 
-        std::cout << std::fixed << std::setprecision(2) << "\rOdometry (x, y, yaw): ("
-                  << pose.x << ", " << pose.y << ", " << pose.yaw << ")      ";
+    usleep(10000);  // 10 Hz
+  }
 
-        usleep(10000); // 10 Hz
-    }
-
-    return 0;
+  return 0;
 }
