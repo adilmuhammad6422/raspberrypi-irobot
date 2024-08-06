661c4e4e007bf8666b519a5d869dafe352f30fed
Adil Muhammad <35853750+adilmuhammad6422@users.noreply.github.com>
2024-08-06 13:44:36 -0400

    updated

--- Roomba/RobotDriver.h
+++ Roomba/RobotDriver.h
@@ -7,11 +7,12 @@
 #include <thread>
 #include <chrono>
 #include <string>
+#include <atomic>
 
 class RobotDriver {
 public:
     RobotDriver(create::RobotModel model, const std::string& port, int baud)
-        : robot_(model), port_(port), baud_(baud) {}
+        : robot_(model), port_(port), baud_(baud), running_(false) {}
 
     bool connect() {
         if (robot_.connect(port_, baud_)) {
@@ -30,6 +31,7 @@ public:
     }
 
     void stop() {
+        running_ = false; // Set the running flag to false to stop the run method
         robot_.drive(0.0, 0.0);
     }
 
@@ -40,10 +42,11 @@ public:
     }
 
     void run() {
+        running_ = true; // Set the running flag to true to start the loop
         driveStraight(0.2);
         bool contact_bumpers[2] = {false, false};
 
-        while (true) {
+        while (running_) {
             contact_bumpers[0] = robot_.isLeftBumper();
             contact_bumpers[1] = robot_.isRightBumper();
 
@@ -60,8 +63,9 @@ public:
     }
 
     void testVirtualWall() {
+        running_ = true; // Set the running flag to true to start the loop
         driveStraight(0.2);
-        while (true) {
+        while (running_) {
             if (robot_.isVirtualWall()) {
                 turn(-0.2, 0.2, 2000); // turn 180 degrees to the left (adjust timing to change)
                 std::cout << "Virtual wall detected. Turning 180 degrees." << std::endl;
@@ -74,6 +78,7 @@ private:
     create::Create robot_;
     std::string port_;
     int baud_;
+    std::atomic<bool> running_; // Atomic flag to control the loop execution
 };
 
 #endif // ROBOT_DRIVER_H
